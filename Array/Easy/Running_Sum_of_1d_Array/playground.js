e5e023b61f69f6739655ebedc4ed17247ca1fd7f
LIU FRANK <youquan.liu@hotmail.com>
2021-01-19 17:14:00 -0500

    four easy array questions

---
+++ Array/Easy/Running_Sum_of_1d_Array/playground.js
@@ -0,0 +1,15 @@
+//Solution one add-on to temp or n-1 in loop
+var runningSum = function (nums) {
+
+}
+
+//Solution two map
+var runningSum2 = function (nums) {
+
+}
+
+
+var nums = [3, 1, 2, 10, 1]
+// Output: [3,4,6,16,17]
+
+console.log(runningSum(nums))
---
+++ Array/Easy/Shuffle_the_Array/playground.js
@@ -0,0 +1,16 @@
+//Solution One, use i+n as second index
+
+var shuffle = function (nums, n) {
+
+}
+
+//Solution Two, use slice to divide nums
+var shuffle2 = function (nums, n) {
+
+}
+
+
+nums = [1, 2, 3, 4, 4, 3, 2, 1];
+
+console.log(shuffle2(nums, 4))
+// Output: [1,4,2,3,3,2,4,1]
