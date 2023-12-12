c58e593f8b0f0cafdef77e77649c37abbe0e2a28
youquanliu <youquan.liu@hotmail.com>
2023-12-11 21:49:19 -0500

    capitalize

---
+++ Exercises/capitalize/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+let capitalize = (str) => {
+  //split str into array seperate by space' '
+  //iterate over each string in the array, and uppercase the first char
+  //join strings to one string and return
+  let arrStr = str.split(" ");
+  let resultArr = [];
+  for (const subStr of arrStr) {
+    resultArr = [
+      ...resultArr,
+      subStr.charAt(0).toUpperCase() + subStr.slice(1),
+    ];
+  }
+  return resultArr.join(" ");
+};
+
+const str = "a lazy, fox";
+console.log("capitalize:", capitalize(str));
--- playground.js
+++ playground.js
@@ -1,8 +1,9 @@
 const obj = { c: 5, a: 3, p: 4, l: 2, e: 2, " ": 2, b: 2, d: 2 };
 const str = "fairy tales";
-
+const str2 = "fairy";
 // for (const [char, count] of str) {
 //   console.log(char, " : ", count);
 // }
 
 console.log(Object.keys(obj), Object.values(obj), Object.entries(obj));
+console.log(str2.slice(1));
