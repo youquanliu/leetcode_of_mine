881cfbd4f64ffa6ba8a9a4de978c12a7d9fd5d76
youquanliu <youquan.liu@hotmail.com>
2024-01-11 18:47:59 -0500

    implement Queue from two Stack

---
+++ Algorithms/Stack/qFromS.js
@@ -0,0 +1,47 @@
+// --- Directions
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stack = require("./index");
+
+class Queue {
+  constructor() {
+    this.s1 = new Stack();
+    this.s2 = new Stack();
+  }
+  add(record) {
+    this.s1.push(record);
+  }
+  remove() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop());
+    }
+    const record = this.s2.data.pop();
+    while (this.s1.peek()) {
+      this.s1.push(this.s2.pop());
+    }
+    return record;
+  }
+  peek() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop());
+    }
+    const record = this.s2.peek();
+    while (this.s1.peek()) {
+      this.s1.push(this.s2.pop());
+    }
+    return record;
+  }
+}
+
+module.exports = Queue;
