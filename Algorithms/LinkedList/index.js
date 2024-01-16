210c393a656b51896b6e236ac3ce21ff85a11709
youquanliu <youquan.liu@hotmail.com>
2024-01-16 17:28:11 -0500

    Finish up basic linkedlist functions

--- Algorithms/LinkedList/index.js
+++ Algorithms/LinkedList/index.js
@@ -16,7 +16,8 @@ class LinkedList {
   insertFirst(data) {
     const node = new Node(data, this.head);
     this.head = node;
-    //or this.head = new Node(data, this.head);
+
+    // this.insetAt(data, 0)
   }
   size() {
     //find the null
@@ -30,6 +31,7 @@ class LinkedList {
   }
   getFirst() {
     return this.head;
+    //return this.getAt(0);
   }
   getLast() {
     if (!this.head) {
@@ -42,6 +44,7 @@ class LinkedList {
       }
       node = node.next;
     }
+    //return getAt(this.size()-1);
   }
   clear() {
     this.head = null;
@@ -70,26 +73,75 @@ class LinkedList {
     previous.next = null;
   }
   insertLast(data) {
-    const newNode = new Node(data, null);
-    if (!this.head) {
-      this.head = newNode;
-      return;
+    let lastNode = this.getLast();
+
+    if (lastNode) {
+      lastNode.next = new Node(data, null);
+    } else {
+      this.head = lastNode;
     }
+  }
+  getAt(index) {
+    let counter = 0;
     let node = this.head;
     while (node) {
-      if (node.next == null) {
-        node.next = newNode;
-        return;
+      if (counter === index) {
+        return node;
       }
+      counter++;
       node = node.next;
     }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const found = this.getAt(index) || null;
+
+    if (found) {
+      let previous = this.getAt(index - 1);
+      previous.next = found.next;
+    } else return null;
+  }
+
+  insertAt(data, index) {
+    //empty
+    if (!this.head) {
+      this.head = new Node(data, null);
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    //handle out of bounds : "|| this.getLast();"
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, this.previous.next);
+    previous.next = node;
   }
 }
 
-const list = new LinkedList();
-list.insertFirst("a");
-list.insertFirst("b");
-list.insertLast("c");
-console.log(list);
-console.log(list.getLast()); // returns node with data 'C'
+const list = new List();
+list.insertFirst("3");
+list.insertFirst("2");
+list.insertFirst("1");
+list.insertAt("Hi", 1);
+list.getAt(1); // returns node with data 'Hi'
+
+// insetFirst(data) => insertAt(data,0)
+// insertLast(data) => insetAt(data,this.size()-1)
+// removeFirst() => removeAt(0)
+// removeLast() => removeAt(this.size()-1)
+// getFirst() => getAt(0)
+// getLast() => getAt(this.size()-1)
+// This means, all left side methods could be replaced by three methods
+// insetAt, size() and getAt, removeAt
+
 module.exports = { Node, LinkedList };
