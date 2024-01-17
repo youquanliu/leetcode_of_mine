a0915a8b06f1ccda908d2184f9afc022617d3f01
youquanliu <youquan.liu@hotmail.com>
2024-01-17 17:28:48 -0500

    linkedlist - circular

---
+++ Algorithms/LinkedList/circular/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a linked list, return true if the list
+// is circular, false if it is not.
+// --- Examples
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+
+function circular(list) {
+  let slow = this.getFirst();
+  let fast = this.getFirst().next;
+
+  //loop
+
+  //fast reach the tail. fast == slow == null
+  // return true
+
+  //fast === slow return false
+
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
